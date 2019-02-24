e17f3708e31329818c87f57542b9fe8e050814b6
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-24 18:58:04 -0500

    added component functionality, remove, add functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
 import FeedList from './components/FeedList'
 
-const VideoPlayer = ({ id }) => {
-  const link = `https://www.youtube.com/embed/${id}?autoplay=1&fs=1&origin=http://localhost:3000`;
-
-  return (
-    <iframe title='yt-video' type="text/html" width="640" height="360"
-      src={link} frameBorder="0"></iframe>
-  );
-}
 
 class App extends Component {
 
@@ -17,11 +9,6 @@ class App extends Component {
       <>
         <h1>Hello World</h1>
 
-          <div className='row'>
-            <div className='mx-auto align-self-center'>
-              <VideoPlayer id='MmOpzbBsr8E' />
-            </div>
-          </div>
 
         <div>
           <FeedList lol={'lol'}></FeedList>
--- src/components/FeedList.js
+++ src/components/FeedList.js
@@ -1,65 +1,97 @@
 import React from 'react';
 import './FeedList.css';
-import { stat } from 'fs';
 
-class FeedEditor extends React.Component{
-    constructor(props){
-      super(props)
-      this.state = {
-        currentUser: {
-          name:'ramon',
-           feed:['music'],
+
+class FeedEditor extends React.Component {
+        constructor(props) {
+            super(props)
+            this.state = {
+                currentUser: {
+                    name: 'ramon',
+                    feed: ['music'],
+                }
+            }
         }
-      }
-    }
-    
-    /*
-      components 
-      FeedList - Pill Badge with click handler
-      Input form - for adding feed to user.
-    */
-    
-   /* APP LOGIC
-   1.pull feed from localStorage 
-   */
-     getFeed(){
-         let user;
-           user = localStorage.getItem('currentUser') ||(this.state.currentUser) ;// grab 
-           if (typeof(user) === 'string'){
-           user =  JSON.parse(user)
-           }
-            this.setState({currentUser:user},()=>{
-            localStorage.setItem('currentUser',JSON.stringify(this.state.currentUser))
-    })
-    }
-      
-   componentDidMount(){
-     this.getFeed()
-     console.log(this.state)
-   }
-   
-   //2.when user adds to their feed -> 
-   handleFeedAdd = (e) => {
-       if (e.keyCode === 13){
-        let newState = this.state;
-    newState.currentUser.feed = newState.currentUser.feed.concat(e.target.value)
-    this.setState({
-      currentUser:newState.currentUser
-    },()=>{
-        localStorage.setItem(`currentUser`,JSON.stringify(this.state.currentUser))
-  })
-       }
-       
-}
 
-   
-  render(){
+        /*
+          components 
+          FeedList - Pill Badge with click handler
+          Input form - for adding feed to user.
+        */
+
+        /* APP LOGIC
+        1.pull feed from localStorage 
+        */
+        getFeed() {
+            let user;
+            user = localStorage.getItem('currentUser') || (this.state.currentUser); // grab 
+            if (typeof (user) === 'string') {
+                user = JSON.parse(user)
+            }
+            this.setState({
+                currentUser: user
+            }, () => {
+                localStorage.setItem('currentUser', JSON.stringify(this.state.currentUser))
+            })
+        }
+
+        componentDidMount() {
+            this.getFeed()
+            console.log(this.state)
+        }
+
+        //2.when user adds to their feed -> 
+        handleFeedAdd = (e) => {
+            if (e.keyCode === 13) {
+                let newState = this.state;
+                newState.currentUser.feed = newState.currentUser.feed.concat(e.target.value)
+                this.setState({
+                    currentUser: newState.currentUser
+                }, () => {
+                    localStorage.setItem(`currentUser`, JSON.stringify(this.state.currentUser))
+                })
+            }
+
+        }
+
+        handleFeedRemove = e => {
+             console.log(e.target.id)
+            let newState = this.state;
+            let newArr = newState.currentUser.feed.slice(0,e.target.id).concat(newState.currentUser.feed.slice(e.target.id+1))
+            newState.currentUser.feed = newArr
+            console.log(newArr)
+            // newState.currentUser.feed = newState.currentUser.feed.slice(e.target.id);
+            this.setState({
+                currentUser: newState.currentUser
+            }, () => {
+                localStorage.setItem(`currentUser`, JSON.stringify(this.state.currentUser))
+            })
+        }
+
+render(){
       return( 
       <>
-      <div className='row offset-6'>
-      <p>{this.state.currentUser.name}</p>
+      <div className='row'>
+      <div className='col col-6'>
       <input type='text'className='input-group mb-3' onKeyDown={this.handleFeedAdd}></input>
 
+      </div>
+        <div className='col col-6'>
+        {
+            this.state.currentUser.feed.map((e,i)=>{
+                return (
+                <>
+                <div className='feedListItem'>
+                <p style={{'display':'inline','marginRight':'20px'}} key={i}  id={i} >{e}</p>
+                <span onClick={this.handleFeedRemove} className="badge badge-dark">X</span>
+                
+                </div>
+                </>
+                )
+            })
+        }
+
+        </div>
       </div>
 
       </>
