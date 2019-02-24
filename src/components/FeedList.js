a581fa87775282877bc19ffb4f4d7ddfcba8f9a7
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-24 16:29:16 -0500

    added default condition for setting default user feed, initial commit

--- src/components/FeedList.js
+++ src/components/FeedList.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './FeedList.css';
 
-const FeedList = props => {
-    return (
-        <>
-        </>
-    )
-}
+class FeedEditor extends React.component{
+    constructor(props){
+      super(props)
+      this.state = {
+        currentUser: {
+          name:'default',
+           feed:['music'],
+        }
+      }
+    }
+    
+    /*
+      components 
+      FeedList - Pill Badge with click handler
+      Input form - for adding feed to user.
+    */
+    
+   /* APP LOGIC
+   1.pull feed from localStorage 
+     getFeed(){
+        user = localStorage.getItem(currentUser) // grab 
+        if(!user){
+            user = this.state.currentUser
+        }
+        this.setState({currentUser:user})
+   }
+   componentDidMount(){
+     this.getFeed()
+   }
+   2.when user adds to their feed -> 
+   handleFeedAdd(e) 
+       newFeed = this.state.feed.concat(e.target.value)
+       this.setState({
+         feed:newFeed
+      },()=>{
+      localstorage.setItem(CurrentUser,this.state.currentUser) 
+     })
+   }
+  
+   
+   */
+  }
 
-export default FeedList;
+  export default FeedEditor;
