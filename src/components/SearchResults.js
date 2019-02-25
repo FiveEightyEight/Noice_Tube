22b54b8d5ed890054a3c5e1008a86f6ff29bbb8c
ositaigwe@pursuit.org <j0h@example.com>
2019-02-25 15:47:12 -0500

    adds styling for the search component

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+import './SearchResults.css';
 
 
 
@@ -14,9 +15,9 @@ const SearchResults = (props) => {
                                 <img src={e.snippet.thumbnails.medium.url} alt={e.snippet.title} key={i}/>
                                 </div>
                                 <div className="col-7">
-                                  <h5>{e.snippet.title}</h5>
-                                    <span>{e.snippet.channelTitle} * {e.snippet.publishedAt}</span>
-                                 <div>{e.snippet.description}</div>
+                                  <h3>{e.snippet.title}</h3>
+                                    <p className="byline">{e.snippet.channelTitle} &#9702; {e.snippet.publishedAt}</p>
+                                    <p>{e.snippet.description}</p>
                                 </div>
                             </div>
                         </>
