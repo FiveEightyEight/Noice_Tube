7402ed90d3575d0d472d3784d6ef4e9884f1addb
Robert Abreu <robertabreu@pursuit.org>
2019-02-26 18:17:51 -0500

    foundation for descriptionTextcomponent

---
+++ src/components/descriptionText.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './descriptionText.css';
+
+const DescriptionText = props => {
+    return (
+        <>
+            <p>
+                <div class="collapse" id="collapseExample">
+                    <div class="card card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                </div>
+                </div>
+
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    Button with data-target
+                </button>
+            </p>
+        </>
+    )
+}
+
+export default DescriptionText;
