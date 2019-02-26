087ea727b1c4300d7e20e5fbb351a7442d4b5862
ositaigwe@pursuit.org <j0h@example.com>
2019-02-25 21:18:32 -0500

    corrects merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import {Route,Switch} from 'react-router-dom'
 import SearchContainer from './containers/SearchContainer'
-
-
-
-=======
 import FeedList from './containers/FeedList'
 import Video from './containers/Video';
-import { Route, Switch } from 'react-router-dom'
->>>>>>> master
+
 
 
 class App extends Component {
@@ -18,8 +12,6 @@ class App extends Component {
     return (
       <>
         <h1>Hello World</h1>
-<<<<<<< HEAD
-
           <div className='row'>
             <div className='mx-auto align-self-center'>
               <VideoPlayer id='MmOpzbBsr8E' />
@@ -28,17 +20,10 @@ class App extends Component {
           <div>
           <Switch>
             <Route exact path='/search/:search_query'><SearchContainer /></Route>
-          </Switch>
-        </div>
-
-=======
-        <div>
-          <Switch>
             <Route path='/video/:video_id' exact component={Video} />
             <Route exact path='/feededitor'><FeedList /></Route>
           </Switch>
         </div>
->>>>>>> master
       </>
     );
   }
