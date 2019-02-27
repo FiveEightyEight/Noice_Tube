01423bb2d1308402cc1ba886236b340c9cc25eff
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-26 22:17:02 -0500

    initial commit, created NavBar component, fixed bug on feed list component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React, { Component } from 'react';
-import {Route,Switch} from 'react-router-dom'
-import SearchContainer from './containers/SearchContainer'
 import FeedList from './containers/FeedList'
-import Video from './containers/Video';
-
-
+import NavBar from './components/NavBar'
+import {Route,Link,Switch} from 'react-router-dom'
 
 class App extends Component {
 
   render() {
     return (
       <>
-          <div>
+      <NavBar></NavBar>
+        <div>
           <Switch>
-            <Route exact path='/search/:search_query'><SearchContainer /></Route>
-            <Route path='/video/:video_id' exact component={Video} />
-            <Route exact path='/feededitor'><FeedList /></Route>
+          <Route exact path='/feededitor'><FeedList /></Route>
           </Switch>
         </div>
+
       </>
     );
   }
 }
 
 export default App;
-
-//<Route exact path='/feededitor'><FeedEditor /></Route>
-
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './NavBar.css'
+
+const NavBar = props => {
+
+    return(<h1>hi</h1>)
+}
+
+export default NavBar;
