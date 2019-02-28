61b8a1ade2bf565dff643d68c2b8f5ed831d67e2
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-27 20:30:50 -0500

    added searchbar component to the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route,Link,Switch} from 'react-router-dom'
-
+import SearchBar from './SearchBar'
 import './NavBar.css'
 
 const NavBar = props => {
@@ -8,16 +8,19 @@ const NavBar = props => {
     return(
     <nav>
         <div className='row navContainer'>
-        <div className='col col-4'>
+        <div className='col'>
         <Link to='/'>Home</Link>
         </div>
-        <div className='col col-4'>
+        <div className='col'>
         <Link to='/feededitor'>Feed Editor</Link>
         </div>
-        <div className='col col-4'>
-        <Link to='/user'>users</Link>
-
+        <div className='col'>
+        <Link to='/user'>Users</Link>
         </div>
+        <div className='col'>
+        <SearchBar  / >
+        </div>
+        
         </div>
         
         
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './SearchBar.css';
+import {withRouter} from 'react-router-dom'
 
-const SearchBar = props => {
-
-    return (
-        <>
-
-        </>
-    )
+class SearchBar extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            input:''
+        }
+    }
+    handleInputChange = e => { 
+        const searchInput = e.target.value;
+            this.setState({input:searchInput})
+    }
+     handleSubmit = e =>{ 
+            if(e.keyCode === 13){
+           const searchInput = e.target.value;
+            this.props.history.push(`/search/${searchInput}`);
+            }
+  
+    }
+    handleClick = e =>{ 
+         if(e.target.value === this.state.input){
+              const searchInput = e.target.value;
+                 this.props.history.push(`/search/${searchInput}`);
+         }
+    }
+    render(){
+        return (
+            <>
+            <form className="form-inline" >
+                <input className="form-control mr-sm-2" type="search" placeholder="Search Here" aria-label="Search" onChange={this.handleInputChange} onKeyDown={this.handleSubmit}/>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.handleClick} value={this.state.input}>Search</button>
+                </form>
+            </>
+        )
+    }
+   
 }
 
-export default SearchBar;
+export default withRouter(SearchBar);
