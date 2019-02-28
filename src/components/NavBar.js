d697049090792692cef39cd01573e17bee3f29d5
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-27 19:27:05 -0500

    created initial routes in navBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {Route,Link,Switch} from 'react-router-dom'
+
 import './NavBar.css'
 
 const NavBar = props => {
 
-    return(<h1>hi</h1>)
+    return(
+    <nav>
+        <div className='row navContainer justify-content-md-center'>
+        <div className='col col-4'>
+        <Link to='/'>Home</Link>
+        </div>
+        <div className='col col-4'>
+        <Link to='/feededitor'>Feed Editor</Link>
+
+        </div>
+        <div className='col col-4'>
+        <Link to='/users'>users</Link>
+
+        </div>
+        </div>
+        
+        
+    </nav>
+    )
 }
 
 export default NavBar;
