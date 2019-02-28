6c0200fdcc204dd8d18c65650c440e73035e83a5
Robert Abreu <robertabreu@pursuit.org>
2019-02-28 14:16:12 -0500

    created a function that parses publishedAt key from API call, needs a string

---
+++ src/services/main.js
@@ -0,0 +1,13 @@
+import * as moment from 'moment';
+
+const formatPublish = (publishedAt) => {
+    // publishedAt needs to be a string
+    // example: "2019-02-08T16:09:24.000Z"
+    if(typeof publishedAt !== 'string') return 'Publish Date Error';
+    const published = moment(publishedAt, 'YYYY-MM-DD')
+    return moment(published).fromNow()
+}
+
+export {
+    formatPublish,
+}
