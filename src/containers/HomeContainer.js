b5fb1d5775cfa38078c8c9de9bfb6895ac6e0027
ositaigwe@pursuit.org <j0h@example.com>
2019-02-27 22:16:05 -0500

    adds draft version of HomeContainer

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -3,68 +3,87 @@ import axios from 'axios';
 import { withRouter } from 'react-router';
 
 
-
 class HomeContainer extends React.Component {
         constructor(props) {
             super(props)
-            this.props.history.listen((location, action) => {
-              })
             
-
             this.state = {
-                currentSeach: '', 
-                params: this.props.match.params.search_query,
-                data: false, 
-                dataSet: [],
-                show: 1,
-                }
+                _isLoaded: false,
+                currentUser: {
+                  name: 'default',
+                  feed: ['music', 'feed', 'podcast'],
+                },
+                show:  1,
+                feedVideos: [
+                    {
+                        returned: false,
+                        dataSet: [],
+                    }
+                ],
+              }
             }
 
         handleClick = (e) => {
            const valueId = e.target.attributes.getNamedItem('data-id').value
            this.props.history.push(`/video/${valueId}`)
-           /*window.history.go(`https://www.youtube.com/embed/${id}?autoplay=1&fs=1&origin=http://localhost:3000`);*/
-            }
-        
+        }
+           /*window.history.go(`https://www.youtube.com/embed/${id}?autoplay=1&fs=1&origin=http://localhost:3000`);
+}*/
 
-        componentDidMount() {   
-            axios({
+        componentDidMount() { 
+            /*return Promise.all(*/
+                this.state.currentUser.feed.map((e,i)=>{
+             axios({
                 method: 'get',
                 url: 'https://www.googleapis.com/youtube/v3/search',
                 params: {
                   part: 'snippet',
-                  maxResults: this.state.show,
+                  maxResults: 1,
                   videoDefinition: 'high',
                   type: 'video',
                   videoEmbeddable: 'true',
-                  key: 'AIzaSyAo6hXtB20Xe0kUbr8bACLmEmAXdEaQGLk',
-                  q: dealWithSpaces(window.location.hash.slice(9)),
+                  key: 'AIzaSyBcCsdu9K95VsD2umeUKsC-Dj2F-GFgs08',
+                  q:  e,
                   pageToken: ''
                 }
-              })
-              .then((response)=>{
-                  this.setState({
-                      data: true,
-                      dataset: this.state.dataSet.push(response) });
-              })
-              .catch((error)=>{
-                  this.setState({
-                      data: false,
+              }) 
+              .then((response => {
+                this.setState({
+                    _isLoaded: true,
+                    feedVideos: this.state.feedVideos.concat({returned: true, dataSet: response}),
                   })
-              });        
-            }
-
+            }))
+             .catch((error)=>{
+                this.setState({
+               feedVideos: this.state.feedVideos.concat({data: false}),
+            })
+          });        
+        })
+    }
+    
+            
 render(){
       return <>
         <div className='container-fluid'>
-        <hr></hr>
-            <div className='row'></div>
             <div className='row'>
-                    <div className="col-2">
+                <h5>Nav Bar</h5>
+            </div>
+            <hr/>
+            <div className='row'>
+                <h4>Hi User</h4>
+            </div>
+            <hr/>
+            <div className='row'>
+                    <div className="col-3">
+                        <h3>FeedBox</h3>
                     </div>
-                    <div className="col-8">
+                    <div className="col-9">
+                    {(!this.state._isLoaded)? <h1>NO LOADING</h1>: 
+                         this.state.feedVideos.slice(1).map((e,i)=>{
+                        return e.returned === false ? <p>No videos found for feed</p> : <p>Explorer</p>
+                        })
+                    }
                     </div>
-                    <div className="col-2"></div>
                 </div>
             </div>
       </>
