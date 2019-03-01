59519379e0f30c7846aad22a1c43f456f7ea9514
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-28 20:54:52 -0500

    added background color change once user is selected

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import './Userlist.css'
 const Userlist = props => { 
+  
     if (props.state.userList === 'undefined'){
         return;
     }
@@ -14,8 +15,8 @@ const Userlist = props => {
                     <ul className="list-group">
                         {userList.map((currUser,i) => {
                             return (
-                            <div key = {i} >
-                                <p  style={{'display':'inline','marginRight':'75%'}}   id={i} onClick={props.handleUserClick} >{currUser.name}</p>
+                            <div  className="user" style= { props.state.currentUser === currUser ? {backgroundColor:'#334FFF'} : {null:null} } key = {i} >
+                                <p  style={{'display':'inline','marginRight':'75%','color':'black'}}   id={i} onClick={props.handleUserClick} >{currUser.name}</p>
                                 <span  id={i} onClick={props.removeUser} className="badge badge-dark">Remove User</span>
                                 </div>
                             )
