1c9115fe8db43a38c6e792a323088e9f35fa49ab
Robert Abreu <robertabreu@pursuit.org>
2019-02-28 20:31:23 -0500

    buildSearchResultObject takes in respoonse.data from a search and returns a parsed version; renamed parseSearch to parseVideo

--- src/services/main.js
+++ src/services/main.js
@@ -8,47 +8,61 @@ const search = (query, count = 10, page = '') => {
         method: 'get',
         url: 'https://www.googleapis.com/youtube/v3/search',
         params: {
-          part: 'snippet',
-          maxResults: count,
-          videoDefinition: 'high',
-          type: 'video',
-          videoEmbeddable: 'true',
-          key: API_KEY,
-          q:  query,
-          pageToken: page,
+            part: 'snippet',
+            maxResults: count,
+            videoDefinition: 'high',
+            type: 'video',
+            videoEmbeddable: 'true',
+            key: API_KEY,
+            q: query,
+            pageToken: page,
         }
-      }); 
+    });
 };
 
 const multiSearch = (array) => {
-    if(array.length <= 0) throw new Error('multiSearch requires at least one query string');
+    if (array.length <= 0) throw new Error('multiSearch requires at least one query string');
     const promiseAll = [];
     for (let i = 0; i < array.length; i++) {
         const query = array[i];
-        if(typeof query !== 'string') throw new Error(`multiSearch requires strings data\nIndex Error: ${i}. Query: `, query);
+        if (typeof query !== 'string') throw new Error(`multiSearch requires strings data\nIndex Error: ${i}. Query: `, query);
         promiseAll.push(search(query));
     }
     return promiseAll;
 };
 
-const parseSearch = (resultObj, simple = true) => {
+const buildSearchResultObject = (data) => {
+    // requires response.data
+    const obj = {}
+    const arr = [];
+    obj['previousPageToken'] = '';
+    obj['nextPageToken'] = data.nextPageToken;
+    for (let i = 0; i < data.items; i ++) {
+        const currentVideo = data.items[i]
+        arr.push(parseVideo(currentVideo));
+    }
+    obj['items'] = arr; 
+    return obj;
+};
+
+const parseVideo = (resultObj, simple = true) => {
     const parsed = {};
     if (simple) {
-    parsed['videoId'] = resultObj.id.videoId;
-    parsed['videoTitle'] = resultObj.snippet.title;
-    parsed['channelId'] = resultObj.snippet.channelId;
-    parsed['channelName'] = resultObj.snippet.channelTitle;
-    parsed['publishedAt'] = resultObj.snippet.publishedAt;
-    parsed['thumbnail'] = resultObj.snippet.thumbnails;
-    parsed['description'] = resultObj.snippet.description;
+        parsed['videoId'] = resultObj.id.videoId;
+        parsed['videoTitle'] = resultObj.snippet.title;
+        parsed['channelId'] = resultObj.snippet.channelId;
+        parsed['channelName'] = resultObj.snippet.channelTitle;
+        parsed['publishedAt'] = resultObj.snippet.publishedAt;
+        parsed['thumbnail'] = resultObj.snippet.thumbnails;
+        parsed['description'] = resultObj.snippet.description;
     } else {
-    parsed['videoId'] = resultObj.id.videoId;
-    parsed['videoTitle'] = resultObj.snippet.title;
-    parsed['channelId'] = resultObj.snippet.channelId;
-    parsed['channelName'] = resultObj.snippet.channelTitle;
-    parsed['publishedAt'] = resultObj.snippet.publishedAt;
-    parsed['thumbnail'] = resultObj.snippet.thumbnails;
-    parsed['description'] = resultObj.snippet.description;
+        parsed['videoId'] = resultObj.id.videoId;
+        parsed['videoTitle'] = resultObj.snippet.title;
+        parsed['channelId'] = resultObj.snippet.channelId;
+        parsed['channelName'] = resultObj.snippet.channelTitle;
+        parsed['publishedAt'] = resultObj.snippet.publishedAt;
+        parsed['thumbnail'] = resultObj.snippet.thumbnails;
+        parsed['description'] = resultObj.snippet.description;
     }
     return parsed;
 };
@@ -58,7 +72,7 @@ const formatPublish = (publishedAt) => {
     // publishedAt needs to be a string
     // example: "2019-02-08T16:09:24.000Z"
     // how to access response.data.items[i].snippet.publishedAt
-    if(typeof publishedAt !== 'string') return 'Publish Date Error';
+    if (typeof publishedAt !== 'string') return 'Publish Date Error';
     const published = moment(publishedAt, 'YYYY-MM-DD')
     return moment(published).fromNow()
 }
