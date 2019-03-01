9c94f5cfc034adb80c05a0f1693152d79f52ca61
Robert Abreu <robertabreu@pursuit.org>
2019-02-28 23:29:59 -0500

    reverted formatPublush

--- src/services/main.js
+++ src/services/main.js
@@ -148,7 +148,7 @@ const formatPublish = (publishedAt) => {
     // publishedAt needs to be a string
     // example: "2019-02-08T16:09:24.000Z"
     // how to access response.data.items[i].snippet.publishedAt
-    if (typeof publishedAt !== 'string') return 'Publish Date Error';
+    if(typeof publishedAt !== 'string') return 'Publish Date Error';
     const published = moment(publishedAt, 'YYYY-MM-DD')
     return moment(published).fromNow()
 }
