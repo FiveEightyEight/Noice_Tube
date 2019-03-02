e9514c4aeac61aab9823cb0e0b4909185d0796f0
Jorgebillini <Jorgebillini@pursuit.org>
2019-03-02 18:32:41 -0500

    minor change to Greeting tag from <p> to <h1>

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Greeting = props => { 
     return (
         <>
-         {props.name === 'default'? 'Hi': <p style={{color:'#000000'}}>{props.name}'s Feed</p>}
+         {props.name === 'default'? 'Hi': <h1 style={{color:'#000000'}}>{props.name}'s Feed</h1>}
         </>
     )
 }
