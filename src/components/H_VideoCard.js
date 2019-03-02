4ffefa2500ebcd970e63e99836984834e3a3115c
ositaigwe@pursuit.org <j0h@example.com>
2019-03-01 20:43:19 -0500

    adds HHCard Component to Search Results

--- src/components/H_VideoCard.js
+++ src/components/H_VideoCard.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './H_VideoCard.css';
 
-const H_VideoCard = props => {
-    return (
-        <>
-        </>
-    )
+
+
+const H_VideoCard = (props) => {
+    console.log("HV",props.results)
+    return <>
+                {
+                    props.results.resultsReturned[0].items.map((e, i) => {
+                        return <Fragment key={i}>
+                            <div className="row">
+                              <div className="col-5">
+                                <img src={e.thumbnail} alt={e.title} data-id={e.videoId} onClick={props.click}/>
+                                </div>
+                                <div className="col-7">
+                                  <h3 data-id={e.videoId} onClick={props.click}>{e.videoTitle}</h3>
+                                    <p className="byline">{e.channelName} &#9702; {e.publishedAt}</p>
+                                    <p data-id={e.videoId} onClick={props.click}>{e.description}</p>
+                                </div>
+                            </div>
+                        </Fragment>
+                        
+                })
+            }
+    </>
 }
+
 
 export default H_VideoCard;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,30 +1,17 @@
 import React, {Fragment} from 'react';
 import { withRouter} from 'react-router';
 import './SearchResults.css';
+import H_VideoCard from './H_VideoCard';
 
 
 
 const SearchResults = (props) => {
-    console.log('B', props.resultsReturned)
     return <>
-                {
-                    props.resultsReturned[0].data.items.map((e, i) => {
-                        return <Fragment key={i}>
-                            <div className="row" key={i}>
-                              <div className="col-5">
-                                <img src={e.snippet.thumbnails.medium.url} alt={e.snippet.title} data-id={e.id.videoId} onClick={props.click}/>
-                                </div>
-                                <div className="col-7">
-                                  <h3 data-id={e.id.videoId} onClick={props.click}>{e.snippet.title}</h3>
-                                    <p className="byline">{e.snippet.channelTitle} &#9702; {e.snippet.publishedAt}</p>
-                                    <p data-id={e.id.videoId} onClick={props.click}>{e.snippet.description}</p>
-                                </div>
-                            </div>
+                 <Fragment key={props.resultsReturned.query}>
+                            <H_VideoCard results={props}></H_VideoCard>
                         </Fragment>
-                        })
-                }
+            
     </>
 }
 
 export default withRouter(SearchResults);
-
