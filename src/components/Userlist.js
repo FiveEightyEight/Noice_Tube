7cde421d23b5513de43de592e474d6dafceb8da9
Robert Abreu <robertabreu@pursuit.org>
2019-03-02 21:43:34 -0500

    CurrentUser is now highlighted when user page is loaded; changed styling to userList render

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import './Userlist.css'
-const Userlist = props => { 
-  
-    if (props.state.userList === 'undefined'){
+const Userlist = props => {
+
+    if (props.state.userList === 'undefined') {
         return;
     }
     else {
-        
-        const userList = props.state.userList    
-    return(
+
+        const userList = props.state.userList
+        return (
             <>
-                <div className = 'container col-7 left'>  
+                <div className='container col-7 left'>
                     <ul className="list-group">
-                        {userList.map((currUser,i) => {
+                        {userList.map((currUser, i) => {
                             return (
-                                <div  className="user row" style= { props.state.currentUser === currUser ? {backgroundColor:'#102542'} : {null:null} } key = {i} id='list' >
-                                  <p className ='col-3'style={{'display':'inline','marginRight':'75%','color':'black'}}   id={i} onClick={props.handleUserClick} >{currUser.name}</p>
-                                  <button data-index={i} onClick={props.removeUser} className="badge badge-dark col-3 removeButton" id='removelink'>Remove User</button>
+                                <div className="user row justify-content-between p-3" style={props.state.currentUser.name === currUser.name ? { backgroundColor: '#102542', 'border': '1px solid #f6f930' } : { backgroundColor: '#fcfcfc', 'border': '1px solid #102542' }} key={i} id='list' >
+                                    <span className='col-7' style={props.state.currentUser.name === currUser.name ? {'color': '#f6f930' }: {'color': '#000000'}} id={i} onClick={props.handleUserClick} >{currUser.name}</span>
+                                    <button className="badge badge-dark col-3 removeButton user" data-index={i} onClick={props.removeUser} id='removelink' style={props.state.currentUser.name === currUser.name ? { 'border': '1px solid #f6f930' } : { 'border': '1px solid #102542' }}>Remove User</button>
                                 </div>
                             )
-                                })}
+                        })}
                     </ul>
                 </div>
             </>
-            )
-        }
-   
+        )
     }
-    
-  
+
+}
+
+
 
 export default Userlist;
