fa2e0acb054d416130f3664caad5100d0946033b
Robert Abreu <robertabreu@pursuit.org>
2019-03-03 10:11:16 -0500

    implemented loadmore to search page

--- src/services/API_KEY.js
+++ src/services/API_KEY.js
@@ -1,3 +1,5 @@
-const API_KEY = 'AIzaSyAdOip0c2sqhoUrcY-e3RPrV61ay4khhOk';
+// const API_KEY = 'AIzaSyAdOip0c2sqhoUrcY-e3RPrV61ay4khhOk';
+// const API_KEY = 'AIzaSyAwUSyfQHZ-olojFZM0w1K7EXZ_lr9crDo';
+const API_KEY = '';
 
 export default API_KEY;
