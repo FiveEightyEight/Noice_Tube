7eafb93ed2cdb9baf52834c022e226d9fc335b74
Jorgebillini <Jorgebillini@pursuit.org>
2019-03-02 21:05:15 -0500

    changed styles for explorerjs, feedlistjs

--- src/components/Explorer.js
+++ src/components/Explorer.js
@@ -4,7 +4,8 @@ import VVideoCard from './VVideoCard'
 
 const Explorer = ( props ) => {
     return (<div className = 'container'>
-                <div className = 'row'>
+                <div className = 'row Explore'>
+                <h1 className='text-capitalize col-12'>{props.query}</h1>
                 {props.results.map((e, i) => {
                         return (<React.Fragment key ={i}>
                                 <VVideoCard e = {e} key={i} click = {props.handleClick}/>
@@ -12,7 +13,7 @@ const Explorer = ( props ) => {
                         })
                 }
                 </div>
-                    <button className='btn btn-primary' onClick={props.clickLoad} data-id={props.query}>SHOW MORE</button>
+                    <button className='btn p-5px rounded' style={{backgroundColor:'#102542',color:'#000000'}} onClick={props.clickLoad} data-id={props.query}>Load More</button>
             </div>
     )
 }
