206684a259896f5941aebd013f002bcfdd81d8ce
Robert Abreu <robertabreu@pursuit.org>
2019-03-03 10:11:48 -0500

    added global api key

--- src/services/API_KEY.js
+++ src/services/API_KEY.js
@@ -1,5 +1,3 @@
-// const API_KEY = 'AIzaSyAdOip0c2sqhoUrcY-e3RPrV61ay4khhOk';
-// const API_KEY = 'AIzaSyAwUSyfQHZ-olojFZM0w1K7EXZ_lr9crDo';
-const API_KEY = '';
+const API_KEY = 'AIzaSyAwUSyfQHZ-olojFZM0w1K7EXZ_lr9crDo';
 
 export default API_KEY;
